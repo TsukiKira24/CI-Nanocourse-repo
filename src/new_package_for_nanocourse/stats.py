f038c7fc98ae7679978dc7c05bac7d7baa7ae744
TsukiKira24 <aleksandra.wosztyl24@gmail.com>
2026-03-06 11:54:08 -0600

    Update stats.py

--- src/new_package_for_nanocourse/stats.py
+++ src/new_package_for_nanocourse/stats.py
@@ -1,6 +1,4 @@
-def mean(values: list[int]) -> float:
+def mean(values: list[float]) -> float:
+    if len(values) == 0:
+        raise ValueError("values must not be empty")
     return sum(values) / len(values)
-
-
-def std_dev(values):
-    pass
