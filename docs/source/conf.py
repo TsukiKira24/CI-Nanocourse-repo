d7135f8a6e9fa91318d7aed0929c1a193950095d
TsukiKira24 <aleksandra.wosztyl24@gmail.com>
2026-03-06 14:59:05 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,154 @@
 # Configuration file for the Sphinx documentation builder.
 #
-# For the full list of built-in configuration values, see the documentation:
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# -- Path setup --------------------------------------------------------------
 
-project = "Nanocourse-lectures"
-copyright = "2026, TeamA+B+C"
-author = "TeamA+B+C"
-release = "today"
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+from navigate import __version__
+import datetime
+
+sys.path.insert(0, os.path.abspath("../../src"))
+
+# -- Project information -----------------------------------------------------
+year = datetime.datetime.now().year
+project = "navigate"
+copyright = f"{year}, Dean Lab, UT Southwestern Medical Center"
+author = "Dean Lab, UT Southwestern Medical Center"
+
+# The full version, including alpha/beta/rc tags
+release = __version__
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.githubpages",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+    "sphinx_toolbox.collapse",
+    "sphinx.ext.autosectionlabel",
+    "sphinx.ext.todo"
+]
 
-templates_path = ["_templates"]
-exclude_patterns = []
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = False
 
-language = "En"
+autosectionlabel_prefix_document = True
+
+# Boolean indicating whether to scan all found documents for
+# autosummary directives, and to generate stub pages for each
+# (http://sphinx-doc.org/latest/ext/autosummary.html)
+autosummary_generate = True
+
+# Both the class’ and the __init__ method’s docstring are concatenated
+# and inserted.
+autoclass_content = "class"  # "both"
+
+# inheritance_graph_attrs = {'rankdir': "TB",
+#                           'clusterrank': 'local'}
+# inheritance_node_attrs  = {'style': 'filled'}
+
+# This value selects how automatically documented members are sorted
+# (http://sphinx-doc.org/latest/ext/autodoc.html)
+autodoc_member_order = "groupwise"
+
+# This value is a list of autodoc directive flags that should be
+# automatically applied to all autodoc
+# directives. (http://sphinx-doc.org/latest/ext/autodoc.html)
+autodoc_default_flags = [
+    "members",
+    "inherited-members",
+    "show-inheritance",
+]
+
+autodoc_inherit_docstrings = True
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["./_templates"]
+
+# The suffix of source filenames.
+source_suffix = ".rst"
+
+# If true, the current module name will be prepended to all
+# description unit titles (such as .. function::).
+add_module_names = True
+
+# The default language to highlight source code
+highlight_language = "python"
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["**/configurations_archive/*", "*archive*", "_templates/*"]
+
+autodoc_mock_imports = [
+    "navigate.model.devices.APIs",
+    "navigate.model.devices.camera.photometrics",
+    "navigate.model.devices.camera.ximea",
+    "navigate.model.devices.camera.daheng",
+    "pyvcam",
+    "ximea",
+]
+
 
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
-html_static_path = ["_static"]
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_show_sphinx = False
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+# html_static_path = ["_static"]
+
+html_logo = "../../src/navigate/view/icon/mic.png"
+
+pygments_style = "sphinx"
+
+# -- Linkcheck Options ---------------------------------------------
+linkcheck_ignore = [
+    r"http://proxy\.your_university\.edu:1234",
+    r"https://proxy\.your_university\.edu:1234",
+]
+
+# -- LaTeX output options ----------------------------------------------------
+
+latex_elements = {
+    "preamble": r"""
+                  \usepackage[utf8]{inputenc}
+                  \usepackage{enumitem}
+                  \setlistdepth{99}
+                  \DeclareUnicodeCharacter{03BC}{$\mu$}
+                  """,
+    "extraclassoptions": "openany,oneside",
+}
+
+latex_documents = [
+    (
+        "index",
+        "navigate.tex",
+        "navigate Documentation",
+        "Dean Lab, UT Southwestern Medical Center",
+        "manual",
+        True,
+    ),
+]
+
+latex_domain_indices = False
